ea10862d313075cec2cb4eac184b5f715181f268
Peter Sims <ragtimesims@gmail.com>
2020-07-25 09:21:10 -0400

    4.16

---
+++ mooc-java-programming-i/part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author petersims
+ */
+public class PaymentCard {
+
+    private double balance;
+
+    public PaymentCard(double balance) {
+        this.balance = balance;
+    }
+
+    public void addMoney(double amount) {
+        if (this.balance + amount > 150) {
+            this.balance = 150;
+        } else if (amount > 0) {
+            this.balance += amount;
+        }
+
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+
+    }
+
+    public String toString() {
+        return "The card has a balance of " + balance + " euros";
+    }
+}
