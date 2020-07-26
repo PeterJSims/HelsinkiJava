7738840edcdc9376560418bab228d28cfecc1257
Peter Sims <ragtimesims@gmail.com>
2020-07-25 22:45:34 -0400

    5.11

--- mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,61 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment < 2.50) {
+            return payment;
+        } else {
+            this.affordableMeals++;
+            this.money += 2.50;
+            return payment - 2.50;
+        }
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        if (card.balance() < 2.50) {
+            return false;
+        } else {
+            this.affordableMeals++;
+            card.takeMoney(2.50);
+            return true;
+        }
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment < 4.30) {
+            return payment;
+        } else {
+            this.heartyMeals++;
+            this.money += 4.30;
+            return payment - 4.30;
+        }
     }
 
+    public boolean eatHeartily(PaymentCard card) {
+        if (card.balance() < 4.30) {
+            return false;
+        } else {
+            this.heartyMeals++;
+            card.takeMoney(4.30);
+            return true;
+        }
+    }
 
     @Override
     public String toString() {
