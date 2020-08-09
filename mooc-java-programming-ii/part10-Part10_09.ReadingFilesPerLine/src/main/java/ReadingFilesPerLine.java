f539ed8ee8be6eb20176a372ec0c3b5b5ed89096
Peter Sims <ragtimesims@gmail.com>
2020-08-09 18:46:42 -0400

    10.10

--- mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -11,7 +11,19 @@ public class ReadingFilesPerLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test the method here
-
+        String fileName = scanner.nextLine();
+        List<String> lines = read(fileName);
+        lines.stream().forEach(line -> System.out.println(line));
     }
-
+    
+    public static List<String> read(String fileName){
+        List<String> contents = new ArrayList<>();
+        
+        try {
+            contents = Files.lines(Paths.get(fileName)).collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return contents;
+    }
 }
--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -11,7 +11,23 @@ public class BooksFromFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Book>books = readBooks("books.txt");
+        System.out.println(books);
 
+    }
+    
+    public static List<Book> readBooks(String fileName){
+        List<Book> books = new ArrayList<>();
+        try {
+            books = Files.lines(Paths.get(fileName))
+                    .map(row -> row.split(","))
+                    .map(parts -> new Book(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), parts[3]))
+                    .collect(Collectors.toList());
+            
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return books;
     }
 
 }
