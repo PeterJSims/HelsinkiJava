506fecb46cbb6c6c8e56bd90b690736eebb0dbfd
Peter Sims <ragtimesims@gmail.com>
2020-10-26 20:14:45 -0400

    redo whole course for practice

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++
@@ -1,9 +0,0 @@
-
-public class YourFirstAccount {
-
-    public static void main(String[] args) {
-        Account account = new Account("Peter", 100.0);
-        account.deposit(20.0);
-        System.out.println(account);
-    }
-}
