f2720a7b6f8c61a21727587dc94b85609cc29a1e
Peter Sims <ragtimesims@gmail.com>
2021-04-11 16:13:29 -0400

    few quick exercises

--- mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,24 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int positives = 0;
+        int total = 0;
+        while (true) {
+            int inputNumber = Integer.valueOf(scanner.nextLine());
+            if (inputNumber == 0) {
+                break;
+            } else {
+                if (inputNumber > 0) {
+                    positives += 1;
+                    total += inputNumber;
+                }
+            }
+        }
+        if (positives == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((total * 1.0) / positives);
+        }
 
     }
 }
